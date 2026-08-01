80e439ce90a5461123f89ede414ea3a96d1aa43e
kumari-velpuri-2004 <kumarivelpuri3162@gmail.com>
2026-08-01 13:54:08 +0530

    Update cicd.groovy

--- vars/cicd.groovy
+++ vars/cicd.groovy
@@ -10,5 +10,5 @@ def buildArtifact()
 
 def deployTomcat(jobname,ip,context)
 {
-  sh "scp /var/lib/jenkins/workspace/${jobname}/webapp/target/webapp.war ubuntu@$(ip):/var/lib/tomcat10/webapps/$(context).war"
+  sh "scp /var/lib/jenkins/workspace/${jobname}/webapp/target/webapp.war ubuntu@${ip}:/var/lib/tomcat10/webapps/${context}.war"
 }
