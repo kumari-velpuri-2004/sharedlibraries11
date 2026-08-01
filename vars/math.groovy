500f06cd86cd672a18e5caa9cb4565aa545f965e
kumari-velpuri-2004 <kumarivelpuri3162@gmail.com>
2026-08-01 12:03:39 +0530

    Add basic arithmetic functions in math.groovy

---
+++ vars/math.groovy
@@ -0,0 +1,18 @@
+def sum(a,b)
+{
+  echo "The sum of $(a) and $(b) is $(a+b)"
+}
+
+def sub(a,b)
+{
+  echo "The subraction of $(a) and $(b) is $(a+b)"
+}
+def mul(a,b)
+{
+  echo "The multification of $(a) and $(b) is $(a*b)"
+}
+
+def div(a,b)
+{
+  echo "The division of $(a) and $(b) is $(a/b)"
+}
